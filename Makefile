a986308c4ab03f7ce5cdd758b393d9ed0781ba52
Dustin Falgout <dustin@falgout.us>
2016-11-23 21:16:32 -0600

    Makefile and install script are now fully functional again.

--- Makefile
+++ Makefile
@@ -1,82 +1,43 @@
 THIS:=$(realpath $(lastword $(MAKEFILE_LIST)))
 REPO_ROOT_DIR:=$(dir $(THIS))
-REPO_DIR_NAME:=$(shell basename $(REPO_ROOT_DIR))
-DIST_DIR:=$(REPO_ROOT_DIR)dist/$(REPO_DIR_NAME)
-SRC_DIR:=$(REPO_ROOT_DIR)src
-
-SRC_DIR_GTK:=$(SRC_DIR)/toolkits/gtk-3.0
-SRC_DIR_GTK320:=$(SRC_DIR)/toolkits/gtk-3.20
-SRC_DIR_CINNAMON:=$(SRC_DIR)/desktops/cinnamon
-SRC_DIR_GNOME:=$(SRC_DIR)/desktops/gnome-shell
-
-DIST_DIR_GTK:=$(DIST_DIR)/gtk-3.0
-DIST_DIR_GTK320:=$(DIST_DIR)/gtk-3.20
-DIST_DIR_CINNAMON:=$(DIST_DIR)/cinnamon
-DIST_DIR_GNOME:=$(DIST_DIR)/gnome-shell
-
-INSTALL_DIR:=$(DESTDIR)/usr/share/themes/$(REPO_DIR_NAME)
-
-COMPILE_RESOURCES:=glib-compile-resources
-SASS:=scss
-SASSFLAGS:=--sourcemap=none
 UTILS:=$(realpath $(REPO_ROOT_DIR)/scripts/utils.sh)
-
-# Hack to make our variables available in utils script without passing them individually.
-MAKE_ENV := $(shell echo '$(.VARIABLES)' | awk -v RS=' ' '/^[a-zA-Z0-9_]+$$/')
-SHELL_EXPORT := $(foreach v,$(MAKE_ENV),$(v)='$($(v))')
+INSTALL_DIR:=$(DESTDIR)/usr/share/themes
 
 
 all: gresource
 
-
 changes:
-	$(UTILS) changes
-
+	$(UTILS) changes $(REPO_ROOT_DIR)
 
 clean:
-	@$(SHELL_EXPORT) $(UTILS) clean
-
+	$(UTILS) clean $(REPO_ROOT_DIR)
 
 create-dist:
-	$(SHELL_EXPORT) $(UTILS) create-dist
-
+	$(UTILS) create-dist $(REPO_ROOT_DIR)
 
 css: clean create-dist
-	@$(SHELL_EXPORT) $(UTILS) css
-
+	$(UTILS) css $(REPO_ROOT_DIR)
 
 _gresource: css
-	@$(SHELL_EXPORT) $(UTILS) _gresource
+	$(UTILS) gresource $(REPO_ROOT_DIR)
 
 gresource: _gresource remove-scss-dist
 
-
 _install:
-	@$(SHELL_EXPORT) $(UTILS) install
+	$(UTILS) install $(REPO_ROOT_DIR) $(INSTALL_DIR)
 
 install: all _install
 
-
 remove-scss-dist:
-	rm -rf $(SRC_DIR_GTK)/dist $(SRC_DIR_GTK320)/dist $(SRC_DIR_CINNAMON)/dist
-
-
-watch: clean
-	while true; do \
-		make gresource; \
-		inotifywait @gtk.gresource -qr -e modify -e create -e delete $(RES_DIR); \
-	done
-
+	$(UTILS) remove-scss-dist $(REPO_ROOT_DIR)
 
 uninstall:
-	rm -rf $(INSTALL_DIR)
-
+	$(UTILS) uninstall $(REPO_ROOT_DIR) $(INSTALL_DIR)
 
 zip: all
 	mkdir $(REPO_ROOT_DIR)/dist
 	$(UTILS) install $(REPO_ROOT_DIR)/dist/$$(basename $(INSTALL_DIR))
 	cd $(REPO_ROOT_DIR)/dist && zip --symlinks -rq $$(basename $(INSTALL_DIR)) $$(basename $(INSTALL_DIR))
-
 
 
 .PHONY: all changes clean create-dist css _gresource gresource
--- scripts/utils.sh
+++ scripts/utils.sh
@@ -1,6 +1,73 @@
 #!/bin/bash
 
+DEST_DIR=''
+DIST_DIR=''
+INSTALL_DIR=''
+REPO_NAME=''
+REPO_ROOT=''
+SRC_DIR=''
 THEME_PARTS=()
+
+DIST_DIR_CINNAMON=''
+DIST_DIR_GNOME=''
+DIST_DIR_GTK=''
+DIST_DIR_GTK320=''
+
+SASS=''
+SASSFLAGS=''
+
+SRC_DIR_CINNAMON=''
+SRC_DIR_GNOME=''
+SRC_DIR_GTK=''
+SRC_DIR_GTK320=''
+
+
+
+
+_initialize_variables() {
+	REPO_ROOT="${1::-1}"
+	DEST_DIR="$2"
+	REPO_NAME=$(basename "${REPO_ROOT}")
+
+	DIST_DIR="${REPO_ROOT}/dist/${REPO_NAME}"
+	INSTALL_DIR="${DEST_DIR}/${REPO_NAME}"
+	SRC_DIR="${REPO_ROOT}/src"
+
+	DIST_DIR_CINNAMON="${DIST_DIR}/cinnamon"
+	DIST_DIR_GNOME="${DIST_DIR}/gnome-shell"
+	DIST_DIR_GTK="${DIST_DIR}/gtk-3.0"
+	DIST_DIR_GTK320="${DIST_DIR}/gtk-3.20"
+
+	SASS='scss'
+	SASSFLAGS='--sourcemap=none'
+
+	SRC_DIR_CINNAMON="${SRC_DIR}/desktops/cinnamon"
+	SRC_DIR_GNOME="${SRC_DIR}/desktops/gnome-shell"
+	SRC_DIR_GTK="${SRC_DIR}/toolkits/gtk-3.0"
+	SRC_DIR_GTK320="${SRC_DIR}/toolkits/gtk-3.20"
+}
+
+
+_output_changes_file_version_marker() {
+
+	line() {
+		dashes="$(printf '%0.s-' $(seq 1 13))"
+		echo "${dashes}>>>> $1 <<<<${dashes}"
+	}
+
+	tag_line="$(line $1)"
+
+	echo "-${tag_line}${tag_line}${tag_line}-"
+}
+
+
+_remove_generated_css_files_and_finalize_dist_dir() {
+	{ rm -f "${DIST_DIR_GTK}"/*.css "${DIST_DIR_GTK320}"/*.css "${DIST_DIR_CINNAMON}"/*.css \
+		&& cp -t "${DIST_DIR_GTK}"      "${SRC_DIR_GTK}"/{*.css,*.png} \
+		&& cp -t "${DIST_DIR_GTK320}"   "${SRC_DIR_GTK320}"/{*.css,*.png,*.theme} \
+		&& cp -t "${DIST_DIR_CINNAMON}" "${SRC_DIR_CINNAMON}"/{*.css,*.json,*.png} \
+		&& cp -t "${DIST_DIR_GNOME}"    "${SRC_DIR_GNOME}"/*.css; }
+}
 
 
 _set_theme_parts_array() {
@@ -24,87 +91,7 @@ _set_theme_parts_array() {
 }
 
 
-_remove_generated_css_files_and_finalize_dist_dir() {
-	{ rm -f "${DIST_DIR_GTK}"/*.css "${DIST_DIR_GTK320}"/*.css "${DIST_DIR_CINNAMON}"/*.css \
-		&& cp -t "${DIST_DIR_GTK}"      "${SRC_DIR_GTK}"/{*.css,*.png} \
-		&& cp -t "${DIST_DIR_GTK320}"   "${SRC_DIR_GTK320}"/{*.css,*.png,*.theme} \
-		&& cp -t "${DIST_DIR_CINNAMON}" "${SRC_DIR_CINNAMON}"/{*.css,*.json,*.png} \
-		&& cp -t "${DIST_DIR_GNOME}"    "${SRC_DIR_GNOME}"/*.css; }
-}
-
-
-do_clean() {
-	rm -rf "${DIST_DIR}"
-}
-
-
-do_create_dist() {
-	echo "${DIST_DIR_GTK}" "${DIST_DIR_GTK320}" "${DIST_DIR_CINNAMON}" "${DIST_DIR_GNOME}"
-	{ mkdir -p "${DIST_DIR_GTK}" "${DIST_DIR_GTK320}" "${DIST_DIR_CINNAMON}" "${DIST_DIR_GNOME}" \
-		&& ln -sf "${SRC_DIR}/common/assets/generated" "${DIST_DIR}/assets"; }
-}
-
-
-do_css() {
-	{ "${SASS}" --update "${SASSFLAGS}" "${SRC_DIR_GTK}/scss":"${SRC_DIR_GTK}/dist" \
-		&& "${SASS}" --update "${SASSFLAGS}" "${SRC_DIR_GTK320}/scss":"${SRC_DIR_GTK320}/dist" \
-		&& "${SASS}" --update "${SASSFLAGS}" "${SRC_DIR_CINNAMON}/scss":"${SRC_DIR_CINNAMON}/dist" \
-		&& cp -t "${DIST_DIR_GTK}" "${SRC_DIR_GTK}"/dist/*.css \
-		&& cp -t "${DIST_DIR_GTK320}" "${SRC_DIR_GTK320}"/dist/*.css \
-		&& cp -t "${DIST_DIR_CINNAMON}" "${SRC_DIR_CINNAMON}"/dist/*.css \
-		&& cp -t "${DIST_DIR_GNOME}" "${SRC_DIR_GNOME}"/scss/*.css; }
-}
-
-
-do__gresource() {
-	{ "${COMPILE_RESOURCES}" --sourcedir="${DIST_DIR}" "${SRC_DIR}/common/${REPO_DIRNAME,,}.gresource.xml" \
-		&& mv "${SRC_DIR}/common/${REPO_DIRNAME,,}.gresource" "${DIST_DIR_GTK}" \
-		&& _remove_generated_css_files_and_finalize_dist_dir; }
-}
-
-
-do_install() {
-	# Copy non-compiled files into DIST_DIR
-	for cp_command_args in "${THEME_PARTS[@]}"
-	do
-		cp_command_args=(${cp_command_args})
-		cp "${cp_command_args[@]}" || return 1
-	done
-
-	# Remove previous install if one exists.
-	[[ -d "${INSTALL_DIR}" ]] && rm -rf "${INSTALL_DIR}"
-
-	# Create INSTALL_DIR path except for the last directory in path.
-	install -dm755 "$(dirname ${INSTALL_DIR})"
-
-	# Remove symlink to assets directory (assets are all in gresource bundle)
-	unlink "${DIST_DIR}/assets"
-
-	# Copy DIST_DIR as INSTALL_DIR
-	cp -r "${DIST_DIR}" "${INSTALL_DIR}"
-
-	# Copy changes, credits, & license to INSTALL_DIR.
-	cp "${REPO_ROOT_DIR}"/{CREDITS,CHANGES,LICENSE} "${INSTALL_DIR}"
-
-	# Create symlink for index.theme in INSTALL_DIR/gtk-3.0
-	( cd "${INSTALL_DIR}/gtk-3.0" && ln -srf ../gtk-3.20/index.theme )
-}
-
-
-output_changes_file_version_marker() {
-
-	line() {
-		dashes="$(printf '%0.s-' $(seq 1 13))"
-		echo "${dashes}>>>> $1 <<<<${dashes}"
-	}
-
-	tag_line="$(line $1)"
-
-	echo "-${tag_line}${tag_line}${tag_line}-"
-}
-
-
-update_changes_file() {
+do_changes() {
 	LAST_STABLE_RELEASE=$(git describe --abbrev=0 --tags $(git rev-list --tags --max-count=1))
 	LAST_MAJOR_MINOR="${LAST_STABLE_RELEASE%.*}"
 
@@ -134,7 +121,7 @@ update_changes_file() {
 
 	[[ -f CHANGES ]] && mv CHANGES CHANGES.old
 
-	output_changes_file_version_marker "${NEXT_STABLE_RELEASE}" > CHANGES
+	_output_changes_file_version_marker "${NEXT_STABLE_RELEASE}" > CHANGES
 
 	{ git log \
 		--pretty=format:"[%ai] %<(69,trunc) %s %><(15) %aN {%h}" \
@@ -149,15 +136,83 @@ update_changes_file() {
 }
 
 
+do_clean() {
+	rm -rf "${DIST_DIR}"
+}
+
+
+do_create_dist() {
+	{ mkdir -p "${DIST_DIR_GTK}" "${DIST_DIR_GTK320}" "${DIST_DIR_CINNAMON}" "${DIST_DIR_GNOME}" \
+		&& ln -sf "${SRC_DIR}/common/assets/generated" "${DIST_DIR}/assets"; }
+}
+
+
+do_css() {
+	{ "${SASS}" --update "${SASSFLAGS}" "${SRC_DIR_GTK}/scss":"${SRC_DIR_GTK}/dist" \
+		&& "${SASS}" --update "${SASSFLAGS}" "${SRC_DIR_GTK320}/scss":"${SRC_DIR_GTK320}/dist" \
+		&& "${SASS}" --update "${SASSFLAGS}" "${SRC_DIR_CINNAMON}/scss":"${SRC_DIR_CINNAMON}/dist" \
+		&& cp -t "${DIST_DIR_GTK}" "${SRC_DIR_GTK}"/dist/*.css \
+		&& cp -t "${DIST_DIR_GTK320}" "${SRC_DIR_GTK320}"/dist/*.css \
+		&& cp -t "${DIST_DIR_CINNAMON}" "${SRC_DIR_CINNAMON}"/dist/*.css \
+		&& cp -t "${DIST_DIR_GNOME}" "${SRC_DIR_GNOME}"/scss/*.css; }
+}
+
+
+do_gresource() {
+	{ glib-compile-resources --sourcedir="${DIST_DIR}" "${SRC_DIR}/common/${REPO_NAME,,}.gresource.xml" \
+		&& mv "${SRC_DIR}/common/${REPO_NAME,,}.gresource" "${DIST_DIR_GTK}" \
+		&& _remove_generated_css_files_and_finalize_dist_dir; }
+}
+
+
+do_install() {
+	# Copy non-compiled files into DIST_DIR
+	for cp_command_args in "${THEME_PARTS[@]}"
+	do
+		cp_command_args=(${cp_command_args})
+		cp "${cp_command_args[@]}" || return 1
+	done
+
+	# Remove previous install if one exists.
+	[[ -d "${INSTALL_DIR}" ]] && rm -rf "${INSTALL_DIR}"
+
+	# Create INSTALL_DIR path except for the last directory in path.
+	install -dm755 "$(dirname ${INSTALL_DIR})"
+
+	# Remove symlink to assets directory (assets are all in gresource bundle)
+	unlink "${DIST_DIR}/assets"
+
+	# Copy DIST_DIR as INSTALL_DIR
+	cp -r "${DIST_DIR}" "${INSTALL_DIR}"
+
+	# Copy changes, credits, & license to INSTALL_DIR.
+	cp "${REPO_ROOT}"/{CREDITS,CHANGES,LICENSE} "${INSTALL_DIR}"
+
+	# Create symlink for index.theme in INSTALL_DIR/gtk-3.0
+	( cd "${INSTALL_DIR}/gtk-3.0" && ln -srf ../gtk-3.20/index.theme )
+}
+
+
+do_remove_scss_dist() {
+	rm -rf "${SRC_DIR_GTK}/dist" "${SRC_DIR_GTK320}/dist" "${SRC_DIR_CINNAMON}/dist"
+}
+
+
+do_uninstall() {
+	rm -rf "${INSTALL_DIR}"
+}
+
+
+_initialize_variables "$2" "$3"
 
 case $1 in
-	_gresource)
-		do__gresource
+	gresource)
+		do_gresource
 		exit $?
 	;;
 
 	changes)
-		update_changes_file
+		do_changes
 		exit $?
 	;;
 
@@ -165,12 +220,12 @@ case $1 in
 		do_clean
 		exit $?
 	;;
-	
+
 	create-dist)
 		do_create_dist
 		exit $?
 	;;
-	
+
 	css)
 		do_css
 		exit $?
@@ -179,6 +234,16 @@ case $1 in
 	install)
 		_set_theme_parts_array
 		do_install
+		exit $?
+	;;
+
+	remove-scss-dist)
+		do_remove_scss_dist
+		exit $?
+	;;
+
+	uninstall)
+		do_uninstall
 		exit $?
 	;;
 
